e6a15ee446dc786b555fdd83b25d56763ca9410a
Saurya <sauryarajdav@gmail.com>
2025-08-02 00:30:43 +0530

    Initial Commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import ReactTypingEffect from 'react-typing-effect';
+import Tilt from 'react-parallax-tilt';
+import profileImage from '../../assets/pic1.jpg';
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+           Saurya Raj
+          </h2>
+          {/* Skills Heading with Typing Effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white"> -- </span>
+            <ReactTypingEffect
+              text={[
+                'Data Structures & Algorithms',
+                'Mernstack',
+                'Machine Learning',
+                'Deep Learning'
+              ,
+                
+              ]}
+              speed={100}
+              eraseSpeed={50}
+              typingDelay={500}
+              eraseDelay={2000}
+              cursorRenderer={(cursor) => (
+                <span className="text-[#8245ec]">{cursor}</span>
+              )}
+            />
+          </h3>
+          {/* About Me Paragraph */}
+          <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            I'm Saurya Raj, a B.Tech student specializing in Artificial Intelligence and Machine Learning at Birla Institute of Technology, Mesra. With a strong foundation in programming, 
+             web development, and machine learning, I enjoy building smart, user-centric solutions that merge creativity with cutting-edge technology. I have hands-on experience 
+            with the MERN stack, Next.js, and AI tools like TensorFlow, PyTorch, and Scikit-Learn.Apart from academics, I've taken leadership roles in the Sports and Adventure Club at BIT Mesra, 
+            and represented my college in inter-institute cricket tournaments.
+          </p>
+          {/* Resume Button */}
+          <a
+            href="https://drive.google.com/file/d/1Xi8M4snVOWZDN6f4Sl7zPRcRL9XUjdAi/view?usp=drivesdk"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: 'linear-gradient(90deg, #8245ec, #a855f7)',
+              boxShadow: '0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec',
+            }}
+          >
+            DOWNLOAD CV
+          </a>
+          
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <Tilt
+            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
+            tiltMaxAngleX={20}
+            tiltMaxAngleY={20}
+            perspective={1000}
+            scale={1.05}
+            transitionSpeed={1000}
+            gyroscope={true}
+          >
+            <img
+              src={profileImage}
+              alt="Saurya Raj"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/constants.js
@@ -0,0 +1,230 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+import PyTorchLogo from './assets/tech_logo/pytorch.png';
+import tensorflowLogo from './assets/tech_logo/tf.png';
+import scikitLearnLogo from './assets/tech_logo/scikit-logo.png';
+import kerasLogo from './assets/tech_logo/keras-logo.png';
+
+
+// Achievement Section Logo's
+import dsalogo from './assets/company_logo/dsa.png';
+import cricketlogo from './assets/company_logo/cricket.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import davLogo from './assets/education_logo/dav.png';
+import jvmLogo from './assets/education_logo/jvm.png';
+import bitLogo from './assets/education_logo/bit.png';
+
+// Project Section Logo's
+import IntelliLiftLogo from './assets/work_logo/IntelliLift.png';
+import LinkupLogo from './assets/work_logo/Linkup.png';
+import RecommendationLogo from './assets/work_logo/recommendation.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+// Experience section Logo's
+
+
+
+export const SkillsInfo = [
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+    
+      { name: 'Python', logo: pythonLogo },
+
+      { name: 'JavaScript', logo: javascriptLogo },
+
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+
+      { name: 'Vercel', logo: vercelLogo },
+
+    ],
+  },
+  {
+    title: 'Machine Learning',
+    skills: [
+      { name: 'PyTorch', logo: PyTorchLogo },
+      { name: 'TensorFlow', logo: tensorflowLogo },
+      { name: 'Scikit-Learn', logo: scikitLearnLogo },
+      { name: 'Keras', logo: kerasLogo },
+
+     
+
+    ],
+  },
+  {
+  title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+
+
+
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+     
+      
+    
+    ],
+  },
+    {
+    title: 'Backend',
+    skills: [
+
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+
+    ],
+  },
+];
+
+
+
+  export const experiences  = [
+    {
+      id: 0,
+      img: dsalogo,
+      role: "Solved 200+ DSA Problems",
+      
+      
+     
+      },
+      
+    {
+      id: 1,
+      img: cricketlogo,
+      role: "Represented BIT Mesra in Inter-Technical Cricket Tournament and secured 1st Position",
+      
+    },
+    {
+      id: 2,
+      img: cricketlogo,
+      role:"Participated in IIT Roorkee's sports fest Sangram as a part of BIT Mesra Cricket Team",
+     
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: bitLogo,
+      school: "Birla Institute of Technology, Ranchi",
+      date: "Nov 2022 - May 2026",
+      grade: "7.42 CGPA",
+      desc: "I am pursuing my Bachelor's degree (B.Tech) in Artificial Intelligence and Machine Learning from BIT Mesra, Ranchi. At BIT, I have gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Artificial Intelligence, and Machine Learning. I have actively participated in various workshops and technical events, which have enhanced my skills and knowledge. My experience at BIT Mesra has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Bachelor of Technology - B.Tech (Artificial Intelligence and Machine Learning)",
+    },
+    {
+      id: 1,
+      img: jvmLogo,
+      school: "JVM Shyamali, Ranchi",
+      date: "March 2022",
+      grade: "84%",
+      desc: "I completed my Senior Secondary education in the Science stream from JVM Shyamali, Ranchi, in March 2022. During my time at the school, I built a strong academic foundation in subjects like Physics, Chemistry, and Mathematics. I also actively participated in school events and science-related activities, which helped nurture my interest in technology and problem-solving.",
+
+      degree: "CBSE(XII) - PCM with Computer Science ",
+    },
+    {
+      id: 2,
+      img: davLogo,
+      school: "DAV Kapildev Public School, Ranchi",
+      date: "March 2020",
+      grade: "90%",
+      desc: "I completed my Secondary education (Class 10) from DAV Kapildev Public School, Ranchi, in 2020. My time at the school helped me develop a strong academic base across core subjects. I also took part in various co-curricular activities, which contributed to my overall growth and interest in learning.",
+      degree: "CBSE(X)",
+    },
+    
+  ];
+
+  export const workexperience  = [
+    {
+      id: 0,
+      img: dsalogo,
+      role: "Solved 200+ DSA Problems",
+    }
+  ];
+  export const projects = [
+    {
+      id: 0,
+      title: "IntelliLift",
+      description:
+      "Built a full-stack AI-powered fitness app using Next.js, React, Tailwind,Shadcn UI, and Convex for real-time data and responsive design.Integrated Voice AI (Vapi) & Gemini LLM to deliver conversational fitness coaching, personalized workout and diet plans.",
+      image: IntelliLiftLogo,
+      tags: ["Next JS", "TailwindCSS", "React JS", "VAPI"],
+      github: "https://github.com/Saurya29/IntelliLift",
+      webapp: "https://intelli-lift.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "LinkUp",
+      description:
+      "Built a full-stack real-time chat app using MERN stack, Socket.io (for messaging & online user status), and Zustand for global state management.Designed responsive UI with Tailwind CSS and DaisyUI, featuring clean components and a seamless user experience across devices.",
+      image: LinkupLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/Saurya29/LinkUp",
+      webapp: "https://github.com/Saurya29/LinkUp",
+    },
+    {
+      id: 2,
+      title: "Medicine Recommendation System",
+      description:
+      "Developed a Flask-based web app using machine learning (Scikit-learn/TensorFlow) to predict diseases and provide personalized medical recommendations.Built a responsive UI with HTML/CSS/JavaScript, ensured data privacy, and used SQLite/MongoDB for scalable storage.", 
+      image: RecommendationLogo,
+      tags: ["Python", "Tensorflow", "Flask", "CSS", "JavaScript"],
+      github: "https://github.com/Saurya29/Medicine_Recommendation",
+      webapp: "N/A",
+    },
+    
+  ];  
